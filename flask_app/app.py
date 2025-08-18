ac880f066e073a58f59867bdc9cc46f1efd89840
iamprashant <iamprashantjain2601@gmail.com>
2025-08-18 14:01:33 +0530

    removed debug from flask app

--- flask_app/app.py
+++ flask_app/app.py
@@ -316,4 +316,5 @@ def generate_trend_graph():
         return jsonify({"error": f"Trend graph generation failed: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)     #for github action runner, debug should be false
