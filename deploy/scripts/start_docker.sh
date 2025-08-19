38c09bebad6e4c80f68a24f92efa6c0e9d3f0245
iamprashant <iamprashantjain2601@gmail.com>
2025-08-19 16:12:34 +0530

    cicd updated with new deployment groups

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,30 +1,69 @@
+# #!/bin/bash
+# set -xe  # Enable command tracing and exit on error
+
+# # Variables
+# AWS_REGION="ap-south-1"
+# AWS_ACCOUNT_ID="739275446561"
+# ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+# ECR_REPO="prashant-mlops-ecr"
+# IMAGE_LOCAL_NAME="prashant-mlops-ecr"
+# IMAGE_TAG="latest"
+# IMAGE_NAME="${ECR_REGISTRY}/${ECR_REPO}:${IMAGE_TAG}"
+# CONTAINER_NAME="prashant-mlops-container"
+
+# echo "Logging in to Amazon ECR..."
+# aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY}
+
+# echo "Stopping and removing any existing container..."
+# docker rm -f ${CONTAINER_NAME} || true
+
+# echo "Pulling latest image from ECR..."
+# docker pull ${IMAGE_NAME}
+
+# echo "Running new container..."
+# docker run -d -p 80:5000 \
+#   -e DAGSHUB_PAT=7bed6b5be2021b1a4eaae221787bcb048ab2bcfd \
+#   --name ${CONTAINER_NAME} \
+#   ${IMAGE_NAME}
+
+# echo "Container started successfully. Logs:"
+# docker logs -f ${CONTAINER_NAME}
+
+
+# ========================================================================================================================
+
+
 #!/bin/bash
 set -xe  # Enable command tracing and exit on error
-
 # Variables
 AWS_REGION="ap-south-1"
 AWS_ACCOUNT_ID="739275446561"
 ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
-ECR_REPO="prashant-mlops-ecr"
-IMAGE_LOCAL_NAME="prashant-mlops-ecr"
+ECR_REPO="test-mlops/yt-chrome-plugin"
+IMAGE_LOCAL_NAME="test-mlops/yt-chrome-plugin"
 IMAGE_TAG="latest"
-IMAGE_NAME="${ECR_REGISTRY}/${ECR_REPO}:${IMAGE_TAG}"
+IMAGE_NAME="${ECR_REGISTRY}:${IMAGE_TAG}"
 CONTAINER_NAME="prashant-mlops-container"
 
+# Step 1: Login to Amazon ECR
 echo "Logging in to Amazon ECR..."
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY}
 
+# Step 2: Stopping and removing any existing container
 echo "Stopping and removing any existing container..."
 docker rm -f ${CONTAINER_NAME} || true
 
+# Step 3: Pulling the latest image from ECR
 echo "Pulling latest image from ECR..."
 docker pull ${IMAGE_NAME}
 
+# Step 4: Running new container
 echo "Running new container..."
 docker run -d -p 80:5000 \
   -e DAGSHUB_PAT=7bed6b5be2021b1a4eaae221787bcb048ab2bcfd \
   --name ${CONTAINER_NAME} \
   ${IMAGE_NAME}
 
+# Step 5: Output container logs
 echo "Container started successfully. Logs:"
 docker logs -f ${CONTAINER_NAME}
